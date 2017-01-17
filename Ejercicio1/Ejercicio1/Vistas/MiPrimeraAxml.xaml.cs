c454604f9cf63b8dc1fa4f417fdd9fa44bdb8a83
Profesor <Profesor@DESKTOP-PT290BD>
2017-01-17 14:21:31 +0100

    Add project files.

---
+++ Ejercicio1/Ejercicio1/Vistas/MiPrimeraAxml.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Ejercicio1.Vistas
+{
+    public partial class MiPrimeraAxml : ContentPage
+    {
+        public MiPrimeraAxml()
+        {
+            InitializeComponent();
+        }
+    }
+}
